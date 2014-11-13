df11594ca1b9be4f9191c0f9b55aac2ebfd49206
Spl3en <spl3en.contact@gmail.com>
2014-11-13 17:35:59 +0100

    Add fail function.

--- dbg.c
+++ dbg.c
@@ -29,6 +29,28 @@ _dbg (char *format, ...)
 }
 
 void
+_fail (char *format, ...)
+{
+	#ifndef NO_COLOR
+	console_set_col(0x0C);
+	#endif
+
+	if (!_output)
+		_output = stdout;
+
+    va_list args;
+
+    va_start (args, format);
+        vfprintf (_output, format, args);
+        fflush (_output);
+    va_end (args);
+
+	#ifndef NO_COLOR
+	console_set_col(0x07);
+	#endif
+}
+
+void
 dbg_set_output (FILE *output)
 {
 	_output = output;
--- dbg.h
+++ dbg.h
@@ -16,14 +16,21 @@
 		#define dbg(format, ...) 	\
 			_dbg("%s:%d| " format "\n", ((strrchr(__FILE__, '\\')) != NULL) ? &(strrchr(__FILE__, '\\'))[1] : __FILE__, __LINE__, ##__VA_ARGS__)
 	#endif
+
+	#ifdef __DEBUG_OBJECT__
+		#define fail(format, ...) 	\
+			_fail(__DEBUG_OBJECT__ ":%d| " format "\n", __LINE__, ##__VA_ARGS__)
+	#else
+		#define fail(format, ...) 	\
+			_fail("%s:%d| " format "\n", ((strrchr(__FILE__, '\\')) != NULL) ? &(strrchr(__FILE__, '\\'))[1] : __FILE__, __LINE__, ##__VA_ARGS__)
+	#endif
 #else
 	#define dbg(format, ...)
+	#define fail(format, ...)
 #endif
 
-void
-_dbg (char *format, ...);
-
-void
-dbg_set_output (FILE *output);
+void _dbg  (char *format, ...);
+void _fail (char *format, ...);
+void dbg_set_output (FILE *output);
 
 // ----------- Methods ------------
