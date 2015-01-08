da169f70d3e9328c1d92ba1991fa5e166119df34
Spl3en <spl3en.contact@gmail.com>
2015-01-08 23:53:41 +0100

    Add warn. Update preprocessor options.

--- dbg.c
+++ dbg.c
@@ -2,7 +2,7 @@
 
 #include <stdarg.h>
 #include <stdio.h>
-#include "Win32Tools/Win32Tools.h"
+#include "Console/Console.h"
 
 FILE *_output = NULL;
 
@@ -10,7 +10,9 @@ void
 _dbg (char *format, ...)
 {
 	#ifndef NO_COLOR
+	#ifdef CONSOLE_COLOR
 	console_set_col(0x02);
+	#endif
 	#endif
 
 	if (!_output)
@@ -24,7 +26,9 @@ _dbg (char *format, ...)
     va_end (args);
 
 	#ifndef NO_COLOR
+	#ifdef CONSOLE_COLOR
 	console_set_col(0x07);
+	#endif
 	#endif
 }
 
@@ -32,7 +36,9 @@ void
 _fail (char *format, ...)
 {
 	#ifndef NO_COLOR
+	#ifdef CONSOLE_COLOR
 	console_set_col(0x0C);
+	#endif
 	#endif
 
 	if (!_output)
@@ -46,7 +52,35 @@ _fail (char *format, ...)
     va_end (args);
 
 	#ifndef NO_COLOR
+	#ifdef CONSOLE_COLOR
 	console_set_col(0x07);
+	#endif
+	#endif
+}
+
+void
+_warn (char *format, ...)
+{
+	#ifndef NO_COLOR
+	#ifdef CONSOLE_COLOR
+	console_set_col(0x0E);
+	#endif
+	#endif
+
+	if (!_output)
+		_output = stdout;
+
+    va_list args;
+
+    va_start (args, format);
+        vfprintf (_output, format, args);
+        fflush (_output);
+    va_end (args);
+
+	#ifndef NO_COLOR
+	#ifdef CONSOLE_COLOR
+	console_set_col(0x07);
+	#endif
 	#endif
 }
 
--- dbg.h
+++ dbg.h
@@ -2,35 +2,45 @@
 
 // ---------- Includes ------------
 #include <stdio.h>
+#include "Utils/Utils.h"
 
 // ---------- Defines -------------
-#define TOSTRING(x) # x
-
 #define __DBG_ACTIVATED__ TRUE
 
 #if __DBG_ACTIVATED__ == TRUE
 	#ifdef __DEBUG_OBJECT__
 		#define dbg(format, ...) 	\
-			_dbg(__DEBUG_OBJECT__ ":%d| " format "\n", __LINE__, ##__VA_ARGS__)
+			_dbg("[" __DEBUG_OBJECT__ "] " format "\n", ##__VA_ARGS__)
 	#else
 		#define dbg(format, ...) 	\
-			_dbg("%s:%d| " format "\n", ((strrchr(__FILE__, '\\')) != NULL) ? &(strrchr(__FILE__, '\\'))[1] : __FILE__, __LINE__, ##__VA_ARGS__)
+			_dbg("[%s] " format "\n", ((strrchr(__FILE__, '\\')) != NULL) ? &(strrchr(__FILE__, '\\'))[1] : __FILE__, ##__VA_ARGS__)
 	#endif
 
 	#ifdef __DEBUG_OBJECT__
 		#define fail(format, ...) 	\
-			_fail(__DEBUG_OBJECT__ ":%d| " format "\n", __LINE__, ##__VA_ARGS__)
+			_fail("[FAIL] [" __DEBUG_OBJECT__ "] " format "\n", ##__VA_ARGS__)
 	#else
 		#define fail(format, ...) 	\
-			_fail("%s:%d| " format "\n", ((strrchr(__FILE__, '\\')) != NULL) ? &(strrchr(__FILE__, '\\'))[1] : __FILE__, __LINE__, ##__VA_ARGS__)
+			_fail("[FAIL] [%s] " format "\n", ((strrchr(__FILE__, '\\')) != NULL) ? &(strrchr(__FILE__, '\\'))[1] : __FILE__, ##__VA_ARGS__)
+	#endif
+
+	#ifdef __DEBUG_OBJECT__
+		#define warn(format, ...) 	\
+			_warn("[" __DEBUG_OBJECT__ "] " format "\n", ##__VA_ARGS__)
+	#else
+		#define warn(format, ...) 	\
+			_warn("[%s] " format "\n", ((strrchr(__FILE__, '\\')) != NULL) ? &(strrchr(__FILE__, '\\'))[1] : __FILE__, ##__VA_ARGS__)
 	#endif
 #else
 	#define dbg(format, ...)
 	#define fail(format, ...)
+	#define warn(format, ...)
 #endif
 
 void _dbg  (char *format, ...);
 void _fail (char *format, ...);
+void _warn (char *format, ...);
+
 void dbg_set_output (FILE *output);
 
 // ----------- Methods ------------
