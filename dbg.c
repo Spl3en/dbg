a2f45417aec1413531357711ed290488a39d5295
Spl3en <spl3en.contact@gmail.com>
2016-04-29 16:39:39 +0200

    Update.

--- dbg.c
+++ dbg.c
@@ -1,1 +1,169 @@
-/** * @license <license placeholder> */// ---------- Includes ------------#include "dbg.h"#include <stdarg.h>#include <stdio.h>#include <string.h>#include <ctype.h>// ------ Structure declaration -------// ------ Static declaration -------// ------ Extern function implementation ------int windows = 1;int enableDebug = 1;void dbg_enableDebug (int enable) {    enableDebug = enable;}int dbg_isEnabled (void) {    return enableDebug;} void dbg_setWindows (int isWindows) {    windows = isWindows; }void _dbg (    int level,    FILE *output,    const char *format,    ...) {    if (!enableDebug) return;    va_list args;    if (windows) {        switch (level) {            // Windows            case DBG_LEVEL_INFO:        SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE), 0x0A); break;            case DBG_LEVEL_DEBUG:       break;            case DBG_LEVEL_WARNING:     SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE), 0x0E); break;            case DBG_LEVEL_ERROR:       SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE), 0x0C); break;            case DBG_LEVEL_IMPORTANT:   SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE), 0x0C); break;            case DBG_LEVEL_SPECIAL:     SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE), 0x0D); break;        }    } else {        switch (level) {            // Linux            case DBG_LEVEL_INFO:      fprintf (output, "\x1b[32m"); break;            case DBG_LEVEL_DEBUG:     fprintf (output, "\x1b[37m"); break;            case DBG_LEVEL_WARNING:   fprintf (output, "\x1b[1;33m"); break;            case DBG_LEVEL_ERROR:     fprintf (output, "\x1b[1;31m"); break;            case DBG_LEVEL_IMPORTANT: fprintf (output, "\x1b[1;31m"); break;            case DBG_LEVEL_SPECIAL:   fprintf (output, "\x1b[1;34m"); break;        }    }    va_start (args, format);        vfprintf (output, format, args);    va_end (args);    if (windows) {        // Windows        SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE), 0x07);    } else {        fprintf (output, "\033[0;0m");    }        fflush (output);}/** * @brief Dump a buffer in the standard output * @param buffer An allocated buffer to dump * @param bufferSize The buffer size * @param prefix A string printed before each line of the dump * @return */void_buffer_print (    void *buffer,    int bufferSize,    const char *prefix,    const char *filename,    int lineNumber,    const char *functionName) {    if (!enableDebug) return;    int curPos = 0;    printf ("[%s:%d in %s] %s ===== buffer size = %d (0x%x) ================\n",        filename, lineNumber, functionName, prefix, bufferSize, bufferSize);    while (curPos < bufferSize) {        int offset;        printf ("[%s:%d in %s] %s", filename, lineNumber, functionName, prefix);        for (offset = 0; offset < 16 && curPos < bufferSize; offset++, curPos++) {            printf (" %02X", ((unsigned char *) buffer)[curPos]);        }        if (offset != 16) {            int j;            for (j = 0; j < 16 - offset; j++) {                printf("   ");            }        }        printf (" | ");        curPos -= offset;        for (offset = 0; offset < 16 && curPos < bufferSize; offset++, curPos++) {            unsigned char c = ((unsigned char *) buffer)[curPos];            printf ("%c", isprint(c) ? c : '.');        }        printf("\n");    }    printf ("[%s:%d in %s] %s ===============================================\n",        filename, lineNumber, functionName, prefix);}
+/**
+ * @license GNU GENERAL PUBLIC LICENSE - Version 2, June 1991
+ *          See LICENSE file for further information
+ */
+
+// ---------- Includes ------------
+#include "dbg.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdint.h>
+
+// ------ Structure declaration -------
+FILE *_output = NULL;
+int dbgTabulations = -1;
+
+// ------ Static declaration -------
+
+// ------ Extern function implementation ------
+
+void dbgSetCustomOutput(char *filename) {
+
+    // Special case : stdout
+    if (strcmp(filename, "stdout") == 0) {
+        dbgSetOutput(stdout);
+        return;
+    }
+
+    FILE *out = fopen(filename, "ab+");
+    if (!out) {
+        error("Cannot open filename '%s'", filename);
+        return;
+    }
+
+    dbgSetOutput(out);
+}
+
+void dbgSetOutput(FILE *output) {
+    _output = output;
+}
+
+void dbgClose(void) {
+    if (_output && _output != stdout && _output != stderr) {
+        fclose(_output);
+    }
+
+    _output = stdout;
+}
+
+void _dbg(int level, char *format, ...) {
+    va_list args;
+
+    if (_output == NULL) {
+        _output = stdout;
+    }
+
+    for (int i = 0; i < dbgTabulations; i++) {
+        fprintf(_output, "  ");
+    }
+
+    switch (level) {
+        #if defined(WIN32) || defined(__CYGWIN__)
+        case DBG_LEVEL_INFO:    SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE), 0x0A); break;
+        case DBG_LEVEL_DEBUG: break;
+        case DBG_LEVEL_WARNING: SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE), 0x0E); break;
+        case DBG_LEVEL_ERROR:   SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE), 0x0C); break;
+        case DBG_LEVEL_SPECIAL: SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE), 0x0B); break;
+        #else
+        case DBG_LEVEL_INFO: fprintf(_output, "\x1b[32m"); break;
+        case DBG_LEVEL_DEBUG: fprintf(_output, "\x1b[37m"); break;
+        case DBG_LEVEL_WARNING: fprintf(_output, "\x1b[33m"); break;
+        case DBG_LEVEL_ERROR: fprintf(_output, "\x1b[31m"); break;
+        case DBG_LEVEL_SPECIAL: fprintf(_output, "\x1b[35m"); break;
+        #endif
+    }
+
+    va_start(args, format);
+        vfprintf(_output, format, args);
+    va_end(args);
+
+    #if defined(WIN32) || defined(__CYGWIN__)
+    SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE), 0x07);
+    #else
+    fprintf(_output, "\033[0m");
+    #endif
+
+    fflush(_output);
+}
+
+void
+_bufferPrint(void *buffer, int bufferSize, char *prefix) {
+
+    int curPos = 0;
+
+    if (_output == NULL) {
+        _output = stdout;
+    }
+
+    fprintf(_output, "%s ===== buffer size = %d (0x%x) ================\n", prefix, bufferSize, bufferSize);
+
+    while (curPos < bufferSize) {
+        int offset;
+        fprintf(_output, "%s", prefix);
+        for (offset = 0; offset < 16 && curPos < bufferSize; offset++, curPos++) {
+            fprintf(_output, " %02X", ((uint8_t *) buffer)[curPos]);
+        }
+        if (offset != 16) {
+            for (int j = 0; j < 16 - offset; j++) {
+                fprintf(_output, "   ");
+            }
+        }
+
+        fprintf(_output, " | ");
+        curPos -= offset;
+
+        for (offset = 0; offset < 16 && curPos < bufferSize; offset++, curPos++) {
+            uint8_t c = ((uint8_t *) buffer)[curPos];
+            fprintf(_output, "%c", isprint(c) ? c : '.');
+        }
+
+        fprintf(_output, "\n");
+    }
+    fprintf(_output, "%s=================================================\n", prefix);
+    fflush(_output);
+}
+
+
+/* Crash handlers */
+#if defined(WIN32) || defined(__CYGWIN__)
+LONG WINAPI crashHandler(EXCEPTION_POINTERS *ExceptionInfo) {
+    die("Application crashed at %p. Exception code = %x",
+         ExceptionInfo->ContextRecord->Rip, ExceptionInfo->ExceptionRecord->ExceptionCode);
+
+    return EXCEPTION_EXECUTE_HANDLER;
+}
+
+#else
+void printTrace(void) {
+    void *array[20];
+    size_t size;
+    char **strings;
+
+    size = backtrace(array, sizeof(array) / sizeof(*(array)));
+    strings = backtrace_symbols(array, size);
+
+    error("Obtained %zd stack frames.", size);
+
+    for (size_t i = 0; i < size; i++) {
+        error("Frame %d : %s", i, strings[i]);
+    }
+
+    error("==================================");
+
+    free(strings);
+}
+
+void crashHandler(int sig, siginfo_t *siginfo, void *_context) {
+    int exceptionCode = siginfo->si_errno;
+    ucontext_t *context = (ucontext_t*) _context;
+    uintptr_t ip = context->uc_mcontext.gregs[REG_RIP];
+
+    if (sig == SIGABRT) {
+        printTrace();
+    }
+
+    die("Application crashed at %p. Exception code = %x", ip, exceptionCode);
+}
+#endif
--- dbg.h
+++ dbg.h
@@ -1,1 +1,172 @@
-/** * @file dbg.h * @brief Provides debug and dump functions * * Provides debug functions with multiple levels and dump utilities. * * @license <license placeholder> */#pragma once// ---------- Includes ------------#include <stdio.h>typedef enum {    DBG_LEVEL_INFO,    DBG_LEVEL_DEBUG,    DBG_LEVEL_WARNING,    DBG_LEVEL_ERROR,    DBG_LEVEL_SPECIAL,    DBG_LEVEL_IMPORTANT} DbgLevel;// ---------- Defines -------------#define __DBG_ACTIVATED__ TRUE/** Get the name of the module object from the filename.    __FILE__ differs between GCC on MinGW and GCC on Linux. */#ifdef WIN32#include <windows.h>#define __FILENAME__1 (((strrchr(__FILE__, '\\')) != NULL) ? &(strrchr(__FILE__, '\\'))[1] : __FILE__)#define __FILENAME__ (((strrchr(__FILENAME__1, '/')) != NULL) ? &(strrchr(__FILENAME__1, '/'))[1] : __FILENAME__1)#else#define __FILENAME__ (((strrchr(__FILE__,  '/')) != NULL) ? &(strrchr(__FILE__,  '/'))[1] : __FILE__)#endif/** Debug line template */#define dbg_ex(level, output, format, ...)                            \    do {                                                              \        _dbg (level, output, "[%s:%d in %s] " format,                 \            __FILENAME__,                                             \            __LINE__,                                                 \            __FUNCTION__,                                             \            ##__VA_ARGS__);                                           \    } while (0)/** Debug line template */#define dbg_ex2(level, output, format, ...)                           \    do {                                                              \        _dbg (level, output, format,                                  \            ##__VA_ARGS__);                                           \    } while (0)/** Buffer dump template */#define buffer_print_ex(buffer, size, prefix)                                \    do {                                                                     \        _buffer_print (                                                      \             buffer,                                                         \             size,                                                           \             prefix,                                                         \            __FILENAME__,                                                    \            __LINE__,                                                        \            __FUNCTION__);                                                   \    } while (0)#if defined(__DBG_ACTIVATED__) && __DBG_ACTIVATED__ == TRUE    // Declare debug functions here    /** Basic level debug function. */    #define dbg(format, ...)                                              \        dbg_ex (DBG_LEVEL_DEBUG, stdout, format "\n", ##__VA_ARGS__)    /** Green printf. */    #define green_printf(format, ...)                                              \        dbg_ex2 (DBG_LEVEL_INFO, stdout, format, ##__VA_ARGS__)    /** Red printf. */    #define red_printf(format, ...)                                              \        dbg_ex2 (DBG_LEVEL_ERROR, stdout, format, ##__VA_ARGS__)    /** Yellow printf. */    #define yellow_printf(format, ...)                                              \        dbg_ex2 (DBG_LEVEL_WARNING, stdout, format, ##__VA_ARGS__)    /** Important level debug function. */    #define important(format, ...)                                              \        dbg_ex (DBG_LEVEL_IMPORTANT, stdout, format "\n", ##__VA_ARGS__)    /** Warning level debug function. */    #define warning(format, ...)                                          \        dbg_ex (DBG_LEVEL_WARNING, stderr, "[WARNING] " format "\n", ##__VA_ARGS__)    /** Error level debug function. */    #define error(format, ...)                                            \        dbg_ex (DBG_LEVEL_ERROR, stderr, "[ERROR] " format "\n", ##__VA_ARGS__)    /** Special level debug function. */    #define special(format, ...)                                            \        dbg_ex (DBG_LEVEL_SPECIAL, stdout, "[SPECIAL] " format "\n", ##__VA_ARGS__)    /** Fatal error level debug function. */    #define die(format, ...)                                              \        do {                                                              \            dbg_ex (DBG_LEVEL_ERROR, stderr, "[FATAL ERROR] " format "\n", ##__VA_ARGS__); \            exit (-1);                                                    \        } while (0)    #define pause() \        do {info ("Pause ... Press any key."); fgetc (stdin);} while (0)    /** Error level debug function. */    #define error_pause(format, ...)                                            \        do {error (format, ##__VA_ARGS__); pause (); } while (0)    /** Error level debug function. */    #define warning_pause(format, ...)                                            \        do {warning (format, ##__VA_ARGS__); pause (); } while (0)    /** Error level debug function. */    #define info_pause(format, ...)                                            \        do {info (format, ##__VA_ARGS__); pause (); } while (0)    /** Dump a buffer into the standard output.     * See _buffer_print for the arguments documentation. */    #define buffer_print(buffer, size, prefix) \        buffer_print_ex (buffer, size, prefix)#else    // Don't output anything if __DBG_ACTIVATED__ is not enabled	#define dbg(format, ...)	#define warning(format, ...)    #define error(format, ...)    #define important(format, ...)	#define die(format, ...)	#define buffer_print(format, ...)    #define special(format, ...)    #define warning_pause(format, ...)    #define info_pause(format, ...)	#define error_pause(format, ...)    #define green_printf(format, ...)    #define pause()#endif/** Info level debug function. Not a debug information */#define info(format, ...)                                          \    dbg_ex (DBG_LEVEL_INFO, stdout, format "\n", ##__VA_ARGS__)// ----------- Functions ------------/** * @brief Output a formated message to a chosen stream * @param level The debug level * @param output A destination stream * @param format the format of the message * @return */void _dbg (    int level,    FILE *output,    const char *format,    ...);/** * @brief Dump a buffer in the standard output * @param buffer An allocated buffer to dump * @param bufferSize The buffer size * @param prefix A string printed before each line of the dump (optional) * @param filename The name of the file * @param lineNumber The line number * @param functionName The function name * @return */void_buffer_print (    void *buffer,    int bufferSize,    const char *prefix,    const char *filename,    int lineNumber,    const char *functionName);void dbg_setWindows (int isWindows);void dbg_enableDebug (int enable);int dbg_isEnabled (void);
+/**
+ * @file dbg.h
+ * @brief Provides debug and dump functions
+ *
+ * Provides debug functions with multiple levels and dump utilities.
+ *
+ */
+
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef enum {
+    DBG_LEVEL_INFO,
+    DBG_LEVEL_DEBUG,
+    DBG_LEVEL_WARNING,
+    DBG_LEVEL_ERROR,
+    DBG_LEVEL_SPECIAL,
+} DbgLevel;
+
+/** Get the name of the module object from the filename. __FILE__ differs between GCC on MinGW and Linux. */
+#define __FILENAME__WIN(file) (((strrchr(file, '\\')) != NULL) ? &(strrchr(file, '\\'))[1] : file)
+#define __FILENAME__NUX(file) (((strrchr(file,  '/')) != NULL) ? &(strrchr(file,  '/'))[1] : file)
+#define GET_FILENAME(file) __FILENAME__WIN(__FILENAME__NUX(file))
+#define __FILENAME__ GET_FILENAME(__FILE__)
+
+#define make_pause()                         \
+    do {                                     \
+        info("Press a key to continue...");  \
+        getc (stdin);                        \
+    } while (0);
+
+/** Debug line template */
+#define dbg_ex(level, output, format, ...)                            \
+    do {                                                              \
+        _dbg(level, "[%s:%d in %s] " format,                          \
+            __FILENAME__,                                             \
+            __LINE__,                                                 \
+            __FUNCTION__,                                             \
+            ##__VA_ARGS__);                                           \
+    } while (0)
+
+#define dbg_exnl(level, output, format, ...)                          \
+    _dbg(level, format, ##__VA_ARGS__);
+
+/** Buffer dump template */
+#define buffer_print_ex(buffer, size, prefix)                         \
+    do {                                                              \
+        char *_meta = NULL; asprintf(&_meta,                          \
+            "[%s:%d in %s] %s",                                       \
+            __FILENAME__,                                             \
+            __LINE__,                                                 \
+            __FUNCTION__,                                             \
+            (prefix) ? prefix : "");                                  \
+            _bufferPrint(buffer, size, _meta);                        \
+            free(_meta);                                              \
+    } while (0)
+
+
+#if defined(__DBG_ACTIVATED__)
+
+    // Declare debug functions here
+    /** Basic level debug function. */
+    #define dbg(format, ...)                                              \
+        dbg_ex(DBG_LEVEL_DEBUG, stdout, format "\n", ##__VA_ARGS__)
+
+    /** Warning level debug function. */
+    #define warning(format, ...)                                          \
+        dbg_ex(DBG_LEVEL_WARNING, stderr, "[WARNING] " format "\n", ##__VA_ARGS__)
+
+    /** Error level debug function. */
+    #define error(format, ...)                                            \
+        dbg_ex(DBG_LEVEL_ERROR, stderr, "[ERROR] " format "\n", ##__VA_ARGS__);
+
+    /** Special level debug function. */
+    #define special(format, ...)                                            \
+        dbg_ex(DBG_LEVEL_SPECIAL, stderr, "[SPECIAL] " format "\n", ##__VA_ARGS__)
+
+    /** Fatal error level debug function. */
+    #define die(format, ...)                                              \
+        do {                                                              \
+            dbg_ex(DBG_LEVEL_ERROR, stderr, "[FATAL ERROR] " format "\n", ##__VA_ARGS__); \
+            make_pause(); \
+            exit (1);                                                     \
+        } while (0)
+
+    /** Dump a buffer into the standard output.
+     * See _bufferPrint for the arguments documentation. */
+    #define buffer_print(buffer, size, prefix) \
+        buffer_print_ex (buffer, size, prefix)
+
+#else
+    // don't output anything except errors if __DBG_ACTIVATED__ is not enabled
+    #define dbg(format, ...)
+    #define warning(format, ...)
+    /** Error level debug function. */
+    #define error(format, ...)                                            \
+        dbg_ex(DBG_LEVEL_ERROR, stderr, "[ERROR] " format "\n", ##__VA_ARGS__)
+    #define die(format, ...)
+    #define buffer_print(buffer, size, prefix) \
+        buffer_print_ex (buffer, size, prefix)
+    #define special(format, ...)
+#endif
+
+/** Structure dissection debug function. */
+#define PRINT_STRUCTURE \
+    for (int __disassDbg = 0; dbgTabulations++, __disassDbg == 0; dbgTabulations -= 2, __disassDbg = 1)
+
+/** Info level debug function. Not a debug information */
+#define info(format, ...)                                          \
+    dbg_ex(DBG_LEVEL_INFO, stdout, format "\n", ##__VA_ARGS__)
+
+// Colored output
+#define info_white(format, ...)                                        \
+    dbg_exnl(DBG_LEVEL_DEBUG, stdout, format, ##__VA_ARGS__)
+
+#define info_green(format, ...)                                        \
+    dbg_exnl(DBG_LEVEL_INFO, stdout, format, ##__VA_ARGS__)
+
+#define info_yellow(format, ...)                                       \
+    dbg_exnl(DBG_LEVEL_WARNING, stdout, format, ##__VA_ARGS__)
+
+#define info_red(format, ...)                                          \
+    dbg_exnl(DBG_LEVEL_ERROR, stdout, format, ##__VA_ARGS__)
+
+#define info_blue(format, ...)                                         \
+    dbg_exnl(DBG_LEVEL_SPECIAL, stdout, format, ##__VA_ARGS__)
+
+/**
+ * @brief Output a formated message to a chosen stream
+ * @param level The debug level
+ * @param output A destination stream
+ * @param format the format of the message
+ * @return
+ */
+void _dbg(int level, char *format, ...);
+
+
+/**
+ * @brief Dump a buffer in the standard output
+ * @param buffer An allocated buffer to dump
+ * @param bufferSize The buffer size
+ * @param prefix A string printed before each line of the dump
+ */
+void _bufferPrint(void *buffer, int bufferSize, char *prefix);
+
+/**
+ * @brief Redirect the default output of the debug messages to a chosen FILE
+ * @param output The destination stream of the debug messages
+ */
+void dbgSetOutput(FILE *output);
+void dbgSetCustomOutput(char *filename);
+
+/**
+ * @brief Close the custom debug file
+ */
+void dbgClose(void);
+
+// crash handlers
+#if defined(WIN32) || defined(__CYGWIN__)
+#include <windows.h>
+LONG WINAPI crashHandler(EXCEPTION_POINTERS *ExceptionInfo);
+#else
+#include <ucontext.h>
+#include <execinfo.h>
+void printTrace(void);
+void crashHandler(int sig, siginfo_t *siginfo, void *_context);
+#endif
+
+// print tabulations
+extern int dbgTabulations;
